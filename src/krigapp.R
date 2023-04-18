aee879b7aa97968c2a5d114e8aee9081a8bb1f52
Moureaux Oscar <130451265+Oscarmour75@users.noreply.github.com>
2023-04-18 18:04:10 +0200

    Random seed button added

--- src/krigapp.R
+++ src/krigapp.R
@@ -3,14 +3,14 @@ library(ggplot2)
 library(rlibkriging)
 library(shinythemes)
 
-ui=fluidPage(
+ui <- fluidPage(
   theme=shinytheme('slate'),
   
   sidebarLayout(
     sidebarPanel(
-      selectInput("kern","Kernel",choices=list(Gaussian="gauss",Exponential="exp",Materntrois="matern3_2",Materncinq="matern5_2")),
+      selectInput("kern", "Kernel", choices=list(Gaussian="gauss",Exponential="exp",Materntrois="matern3_2",Materncinq="matern5_2")),
       sliderInput(inputId='N',label='Number of data',value=5,min=2,max=10,step=1),
-      selectInput("Opt","Optimisation method",choices=c("BFGS","Newton","none")),
+      selectInput("opt", "Optimisation method", choices=list(BFGS="BFGS",Newton="Newton",None="none")),
       conditionalPanel(
         condition = "input.Opt == 'none'",
         sliderInput(inputId='theta',label='Range',value=0.2,min=0,max=1,step=0.001),
@@ -21,22 +21,30 @@ ui=fluidPage(
         sliderInput(inputId='thetaopt',label='Initial range',value=0.2,min=0,max=1,step=0.001),
         sliderInput(inputId='sigopt',label='Initial standard deviation',value=0.5,min=0,max=1,step=0.001),
         actionButton("bout", "Parameters estimation"),
-        radioButtons("est","Estimation method",list(LogLikelihood="LL",LeaveOneOut="LOO",LogMarginalPosterior="LMP")),
-        print("No nugget effect allowed for LOO"),
+        radioButtons("est", "Estimation method", list(LogLikelihood="LL",LeaveOneOut="LOO",LogMarginalPosterior="LMP")),
         
         conditionalPanel(
+          condition = "input.est == 'LOO'",
+          tags$div(checkboxInput("P1", "Nugget effect", value = FALSE), 
+                   style = "pointer-events: none; opacity: 0.5;"),
+          
+        ),
+        conditionalPanel(
           condition = "input.est != 'LOO'",
-          checkboxInput(inputId='P', "Nugget effet",value=FALSE),
+          checkboxInput("P", "Nugget effect", value = FALSE),
           conditionalPanel(
             condition = "input.P == true",
             sliderInput("par", "Nugget parameter", min = 0, max = 1, value = 0.5,step=0.001)
+            ,
+            actionButton("seed", "Change random seed"),
+            
+            
           )
-        ),
-        sliderInput("seed", "Random seed", min = 0, max = 100, value = 0.5,step=1)
-        
+          
+        )
+
       )
-    )
-    ,
+    ),
     mainPanel(
       h1(titlePanel("Kriging")),
       textInput("fonc", "Function to interpolate : f(x)=", value = "1 - 1 / 2 * (sin(12 * x) / (1 + x) + 2 * cos(7 * x) * x^5 + 0.7)"),
@@ -46,13 +54,20 @@ ui=fluidPage(
       h3(textOutput("mod3")),
       h4(textOutput("mod2")),
       h4(textOutput("mod")),
-      h5("Author : Moureaux Oscar")
+      h5("Author : Moureaux Oscar"),
+
     )
   )
   
 )
 server=function(input,output,session){
-
+  
+  seedrec <- reactiveVal(1)
+  
+  observeEvent(input$seed, {
+    seedrec(sample(1:100000, 1))
+  })
+  
   ################################################################
   ########INITIALISATION DES VARIABLES REACTIVES##################
   n=101
@@ -72,18 +87,19 @@ server=function(input,output,session){
   })
   
   k_R <- reactive({
-    Kriging(y(), X(),optim=input$Opt,kernel = input$kern,parameters=parameters(),objective=input$est)
+    
+    Kriging(y(), X(),optim=input$opt,kernel = input$kern,parameters=parameters(),objective=input$est)
   })
   
   ynugg <- reactive({
-    set.seed(input$seed)
+    set.seed(seedrec())
     ma_fonction()(X()) + input$par/5 * rnorm(nrow(X()))
   })
   
   k_Rnugg <- reactive({
-    set.seed(input$seed)
+    set.seed(seedrec())
     
-    NuggetKriging(ynugg(), X(),optim=input$Opt,kernel=input$kern,parameters=parameters(),objective=input$est)
+    NuggetKriging(ynugg(), X(),optim=input$opt,kernel=input$kern,parameters=parameters(),objective=input$est)
   })
   
   ma_fonction <- reactive({
@@ -92,26 +108,27 @@ server=function(input,output,session){
     return(func)
   })
   
-
+  
   
   output$Noy=renderText({
     paste("Choosen Kernel :",input$kern)
   }) 
   
   kR0 <- reactive({
-    Kriging(y(), X(),optim=input$Opt,kernel = input$kern,objective=input$est)
+    set.seed(seedrec())
+    Kriging(y(), X(),optim=input$opt,kernel = input$kern,objective=input$est)
   })
   
   kR0nugg <- reactive({
-    set.seed(input$seed)
-    NuggetKriging(ynugg(), X(),optim=input$Opt,kernel = input$kern,objective=input$est)
+    set.seed(seedrec())
+    NuggetKriging(ynugg(), X(),optim=input$opt,kernel = input$kern,objective=input$est)
     
   })
   
   
-################################################################
-########CHANGEMENT DES PARAMETRES SI APPUI DU BOUTON############
-
+  ################################################################
+  ########CHANGEMENT DES PARAMETRES SI APPUI DU BOUTON############
+  
   observeEvent(input$bout, {
     if(input$P==FALSE) {
       updateSliderInput(session, "thetaopt", value = kR0()$theta()[1])
@@ -123,18 +140,19 @@ server=function(input,output,session){
     }
   })
   
-
-################################################################
-########################     PLOT      #########################
+  
+  
+  ################################################################
+  ########################     PLOT      #########################
   
   output$vec <- renderPlot({
     input$bout
-    input$run
+    input$seed
     input$run2
     isolate({
       
       if(input$P==FALSE) {
-        set.seed(input$seed)
+        set.seed(seedrec())
         
         p <- predict(k_R(), x, TRUE, FALSE)
         
@@ -142,7 +160,7 @@ server=function(input,output,session){
         points(X(), y(),col="red")
         lines(x, p$mean, col = 'blue')
         polygon(c(x, rev(x)), c(p$mean - 2* p$stdev, rev(p$mean + 2 * p$stdev)), border = NA, col = rgb(0, 0, 1, 0.2))
-        set.seed(input$seed)
+        set.seed(seedrec())
         
         s <- simulate(k_R(), nsim = 10, seed = 123, x = x)
         matplot(x, s, col = rgb(0, 0, 1, 0.2), type = 'l', lty = 1, add = TRUE)
@@ -150,7 +168,7 @@ server=function(input,output,session){
       } 
       
       if(input$P==TRUE) {
-        set.seed(input$seed)
+        set.seed(seedrec())
         
         p <- predict(k_Rnugg(), x, TRUE, FALSE)
         
@@ -158,15 +176,15 @@ server=function(input,output,session){
         points(X(), ynugg(),col="red")
         lines(x, p$mean, col = 'blue')
         polygon(c(x, rev(x)), c(p$mean - 2* p$stdev, rev(p$mean + 2 * p$stdev)), border = NA, col = rgb(0, 0, 1, 0.2))
-        set.seed(input$seed)
+        set.seed(seedrec())
         
         s <- simulate(k_Rnugg(), nsim = 10, seed = 123, x = x)
         matplot(x, s, col = rgb(0, 0, 1, 0.2), type = 'l', lty = 1, add = TRUE)
-      
+        
       } 
     })
   })
-
+  
   
   ################################################################
   ########AFFICHAGE DES PARAMETRES EN COURS############
@@ -177,28 +195,36 @@ server=function(input,output,session){
   output$mod2 <- renderText({
     
     input$bout
-    input$run
+    input$seed
     input$run2
     isolate({
-      
-    paste("The estimated final range is :", round(k_R()$theta(), 3), sep = " ")
-      
+      if (input$P){
+        paste("The estimated final range is :", round(kR0nugg()$theta(), 3), sep = " ")
+      }
+      else{
+        paste("The estimated final range is :", round(kR0()$theta(), 3), sep = " ")
+      }
     })
+    
+    
   })
   
   output$mod <- renderText({
     
     input$bout
-    input$run
+    input$seed
     input$run2
     isolate({
-      
-    paste("The estimated final deviation is :", round(k_R()$sigma2(),3), sep = " ")
+      if (input$P){
+        paste("The estimated final deviation is :", round(kR0nugg()$sigma2(),3), sep = " ")
+      }
+      else{
+        paste("The estimated final deviation is :", round(kR0()$sigma2(),3), sep = " ")
+      }
+    })
   })
-  })
-
+  
 }
 
 shinyApp(ui=ui, server=server)
-
 
